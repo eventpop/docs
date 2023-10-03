d60828d47b99f35a68675c2e1468c2593a27daef
Manassarn Manoonchai <manassarn@gmail.com>
2023-10-03 12:02:04 +0700

    Include Eventpop API json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,41 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+import { generateAPI } from "starlight-openapi"
+
+// Generate the documentation and get the associated sidebar groups.
+const { openAPISidebarGroups, starlightOpenAPI } = await generateAPI([
+  {
+    base: "api",
+    label: "API Reference",
+    schema: process.env.API_SCHEMA ?? "./schemas/public.json",
+  },
+])
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: "Eventpop Public API Documentation",
+      social: {
+        github: "https://github.com/eventpop",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+        ...openAPISidebarGroups,
+      ],
+    }),
+
+    // Add the Starlight OpenAPI integration.
+    starlightOpenAPI(),
+  ],
+})
